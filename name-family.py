24280c4b558cb8d24f56d2767f01179c86cb86e0
falon3 <scheers@ualberta.ca>
2016-01-06 18:09:11 -0700

    add first file

---
+++ name-family.py
@@ -0,0 +1,14 @@
+class Student:
+	def __init__(self, name, family):
+		self._name = name
+		self._family = family
+
+	def addCourseMark(self,course,mark):
+		self.courseMarks[course] = mark
+
+	def average(self):
+		
+
+
+
+
